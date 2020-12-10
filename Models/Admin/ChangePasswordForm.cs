c5ee9dcddfa18c6e39740a9f0bb74151bcaf528a
Drake Perrior-Small <drakecsmall@gmail.com>
2020-12-09 18:12:11 -0700

    add change pw constructor

--- Models/Admin/ChangePasswordForm.cs
+++ Models/Admin/ChangePasswordForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +8,9 @@ namespace PAK_www.Models.Admin
 {
     public class ChangePasswordForm : LoginForm
     {
+        public ChangePasswordForm(IConfiguration config)
+            : base(config) { };
+
         public string NewPassword { get; set; }
     }
 }
